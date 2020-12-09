10d0cd9327c7c1f52288ca770345fb7c019e5934
Sameer0503 <shubhamrbagade97@gmail.com>
2020-12-09 14:53:41 +0530

    Roles and permissions with relations added

---
+++ database/migrations/2020_12_09_061509_create_permission_role_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->primary(['role_id','permission_id']);
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->foreignId('permission_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('permission_role', function (Blueprint $table) {
+            Schema::dropIfExists('permission_role');
+        });
+    }
+}
